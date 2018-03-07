ccbb30fc73fd05d17fed0c5dc81b1cff0346e37a
Manish Khatri <m.khatri153@gmail.com>
2018-03-07 11:54:10 +0545

    Add files via upload

---
+++ feedback.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bhuvaneshwor</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+	<link rel="stylesheet" href="owl/owl.carousel.min.css">
+	<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+	<link href="lightbox/lightbox.min.css" rel="stylesheet">
+</head>
+<body class="export-link">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<?php include 'includes/header.php' ?>
+
+<main>
+	<div class="center clearfix">
+
+		<article>
+				<h1>
+					<span>
+						<ins>Write to us</ins>
+						
+					</span>
+					<strong>
+						Feedback. inquiry .order
+					</strong>
+				</h1>
+
+				<section>
+					<form>
+						<div>
+							<input type="text" size="25" placeholder="full name:">
+							<input type="text" size="35" placeholder="Address:">
+						</div>
+						<div>
+							<input type="text" size="20" placeholder="Country:">
+							<input type="text" size="35" placeholder="Company:">
+						</div>
+						<div>
+							<input type="text" size="45" placeholder="E-mail address:">
+							<input type="text" size="20" placeholder="phone/mobile:">
+						</div>
+						<div>
+							<select>
+								<option value="subject">subject</option>
+								<option value="subject">feedback</option>
+								<option value="subject">inquiry</option>
+								<option value="subject">order</option>
+							</select>
+						</div>
+						<div>
+							<textarea type="text" placeholder="message"></textarea>
+						</div>
+
+						<a href="#">send message</a>
+
+					</form>
+				</section>	
+
+			</article>
+		
+
+
+
+		<?php include 'includes/aside.php' ?>
+		
+			
+		
+	</div>
+</main>
+
+<?php include 'includes/footer.php' ?>
+
+</body>
+</html>
